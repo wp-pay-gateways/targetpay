9447235fd12556292743dd670c3b3ce61fe5e820
Remco Tolsma <info@remcotolsma.nl>
2014-07-16 16:42:06 +0200

    Added config and config factory class.

---
+++ src/Config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Title: TargetPay config
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2014
+ * Company: Pronamic
+ * @author Remco Tolsma
+ * @version 1.0.0
+ */
+class Pronamic_WP_Pay_Gateways_TargetPay_Config extends Pronamic_WP_Pay_GatewayConfig {
+	public $layoutcode;
+}
