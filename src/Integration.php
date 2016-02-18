148d04c405499e9e400dec99525b7967a497592d
Remco Tolsma <info@remcotolsma.nl>
2016-02-18 14:04:32 +0100

    Added get settings function.

--- src/Integration.php
+++ src/Integration.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Title: TargetPay integration
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ *
+ * @author Remco Tolsma
+ * @version 1.0.7
+ * @since 1.0.0
+ */
 class Pronamic_WP_Pay_Gateways_TargetPay_Integration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
 	public function __construct() {
 		$this->id            = 'targetpay-ideal';
@@ -22,5 +32,20 @@ class Pronamic_WP_Pay_Gateways_TargetPay_Integration extends Pronamic_WP_Pay_Gat
 
 	public function get_gateway_class() {
 		return 'Pronamic_WP_Pay_Gateways_TargetPay_Gateway';
+	}
+
+	/**
+	 * Get required settings for this integration.
+	 *
+	 * @see https://github.com/wp-premium/gravityforms/blob/1.9.16/includes/fields/class-gf-field-multiselect.php#L21-L42
+	 * @since 1.0.7
+	 * @return array
+	 */
+	public function get_settings() {
+		$settings = parent::get_settings();
+
+		$settings[] = 'targetpay';
+
+		return $settings;
 	}
 }
