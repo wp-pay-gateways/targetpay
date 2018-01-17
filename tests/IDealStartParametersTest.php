9de3c5731560ae8159060cde330f681bee4b0842
Reüel van der Steege <reuel@pronamic.nl>
2018-01-17 12:06:59 +0100

    Happy 2018.

--- tests/IDealStartParametersTest.php
+++ tests/IDealStartParametersTest.php
@@ -3,7 +3,7 @@
 /**
  * Title: TargetPay iDEAL start parameters test
  * Description:
- * Copyright: Copyright (c) 2005 - 2017
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Remco Tolsma
